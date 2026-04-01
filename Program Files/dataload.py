2d744cebe09977cec6e153a66ce94bca4edc639f
Thomas Mendez <tmendez7524@floridapoly.edu>
2026-04-01 18:11:13 -0400

    Updated py files

---
+++ Program Files/dataload.py
@@ -0,0 +1,105 @@
+#load csv file from data folder and upload it into the database
+import oracledb
+import csv
+
+# --- SETUP ---
+LIB_DIR = r"C:\Users\screa\Desktop\College\Year 2\Spring\Databases\Project\Program Files\instantclient-basiclite-windows.x64-23.26.1.0.0\instantclient_23_0"  # Your Instant Client Path
+DB_USER = "Bike_Share"
+DB_PASS = "bikeShare123"
+DB_DSN  = "127.0.0.1:1521/xe"
+
+# Initialize Thick Mode (Required for FreeSQL/Cloud)
+oracledb.init_oracle_client(lib_dir=LIB_DIR)
+
+#Load python data
+def bulk_load_csv(file_path, userInput):
+    try:
+        
+
+        # 1. Connect
+        conn = oracledb.connect(user=DB_USER, password=DB_PASS, dsn=DB_DSN)
+        cursor = conn.cursor()
+
+        # 2. Read CSV Data into a List
+        with open(file_path, mode='r', encoding='utf-8') as f:
+            reader = csv.reader(f)
+            next(reader)  # Skip the header row
+            data_to_insert = [row for row in reader]
+
+        # 3. Prepare Bulk Insert SQL
+        # :1 and :2 correspond to the values in each row of your list
+        # userInput determines which command to run
+        sql = "INSERT INTO "
+        match userInput:
+            case 1:
+                sql += "station (station_id, name, dock_count, installation_date) VALUES (:1, :2, :3, TO_DATE(:4, 'mm/dd/yyyy'))"
+            case 2:
+                sql += "station_location (station_id, latitude, longitude, city) VALUES (:1, :2, :3, :4)"
+            case 3:
+                sql += "status (status_id, station_id, time, bikes_avaialbe, docks_avaible) VALUES (:1, :2, TO_DATE(:3, 'mm/dd/yyyy hh:mi:ss'), :4, :5)"
+            case 4:
+                sql += "trip (trip_id, bike_id, duration, start_station_id, end_station_id, start_date, end_date) "
+                sql += "VALUES (:1, :2, :3, :4, :5, TO_DATE(:6, 'mm/dd/yyyy hh:mi:ss'), TO_DATE(:7, 'mm/dd/yyyy hh:mi:ss'))"
+            case 5:
+                sql += "\"User\" (bike_id, user_type, user_birth_year, user_gender) VALUES (:1, :2, :3, :4)"
+            case 6:
+                sql += "weather (\"Date\", max_temp_f, min_temp_f, max_vis_miles, events) VALUES (TO_DATE(:1, 'mm/dd/yyyy'), :2, :3, :4, :5)"
+            case 7:
+                sql += "concatenateTimeFromDate (date_no_time, date_time) VALUES (TO_DATE(:1, 'mm/dd/yyyy'), TO_DATE(:2, 'mm/dd/yyyy hh:mi:ss'))"
+            case _:
+                print ("Error: improper input given")
+
+        
+
+        # 4. Execute Batch
+        print(f"Starting bulk load of {len(data_to_insert)} rows...")
+        cursor.executemany(sql, data_to_insert)
+        
+        # 5. Commit Changes
+        conn.commit()
+        print(f"Successfully loaded {cursor.rowcount} rows into the database.")
+
+    except Exception as e:
+        print(f"Error during bulk load: {e}")
+        if 'conn' in locals():
+            conn.rollback() # Undo changes if an error occurs
+
+    finally:
+        if 'cursor' in locals(): cursor.close()
+        if 'conn' in locals(): conn.close()
+
+
+print("Which CSV do you wish to load\nstation (1)\nstation_location (2)\nstatus (3)")
+print("trip (4)\nuser (5)\nweather(6)\nall (7)")
+
+userInput = int(input(">> "))
+
+
+filePath = ""
+match userInput:
+    case 1:
+        filePath = 'clean_data/station.csv'
+    case 2:
+        filePath = 'clean_data/station_location.csv'
+    case 3:
+        filePath = 'clean_data/status.csv'
+    case 4:
+        filePath = 'clean_data/trip.csv'
+    case 5:
+        filePath = 'clean_data/user.csv'
+    case 6:
+        filePath = 'clean_data/weather.csv'
+    case 7:
+        filePath = 'clean_data/connect_Weather_Status.csv'
+    case _:
+        print("unknown case")
+
+if userInput != 7:
+    bulk_load_csv(filePath, userInput)
+elif userInput == 7 :
+    bulk_load_csv('clean_data/station.csv', 1)
+    bulk_load_csv('clean_data/station_location.csv', 2)
+    bulk_load_csv('clean_data/status.csv', 3)
+    bulk_load_csv('clean_trip.csv', 4)
+    bulk_load_csv('clean_user.csv', 5)
+    bulk_load_csv('clean_weather.csv', 6)
--- Program Files/preprocess.py
+++ Program Files/preprocess.py
@@ -1,7 +1,190 @@
+import pandas as pd
+
+
+
+CSVColumns = [['id', 'name', 'dock_count', 'installation_date'],
+                ['id', 'lat', 'long', 'city'],
+                ['status_id','station_id', 'time', 'bikes_available', 'docks_available'],
+                ['id', 'bike_id', 'duration', 'start_station_id', 'end_station_id', 'start_date', 'end_date'],
+                ['bike_id', 'user_type', 'member_birth_year','member_gender'],
+                ['date', 'max_temperature_f', 'min_temperature_f', 'max_visibility_miles', 'events']]
 #Take a master csv file and convert it to
 #station.csv, station_location.csv, status.csv, weather.csv, trip.csv, user.csv
 
 # Multiple intake options: 
-# # one giant csv to make all other csvs
-# # smaller large csvs to user determined small csv
+# # trim smaller csv files to what is needed
 
+# Convert strings to integers or floats
+def convertToNum(columnName, df):
+    df[columnName] = pd.to_numeric(df[columnName], errors='coerce')
+    
+    #drop empty values and values that couldnt be converted to int
+    df = df.dropna(subset=[columnName])
+    
+    return df
+
+# Convert strings to mm/dd/yyyy and HH:MM:SS if needed
+    # seconds is a boolean to check if seconds should be included or not
+def convertToDate(columnName, df, seconds):
+    df[columnName] = pd.to_datetime(df[columnName], errors='coerce')
+
+    #drop empty values and values that couldnt be converted to int
+    df = df.dropna(subset=[columnName])
+
+    #Convert to mm/dd/yyyy with or without hr:min:sec
+    if seconds:
+        df[columnName] = df[columnName].dt.strftime('%m/%d/%Y %I:%M:%S')
+    else:
+        df[columnName] = df[columnName].dt.strftime('%m/%d/%Y')
+    return df
+
+def emptyToNaN(columnName, df):
+    df[columnName] = df[columnName].replace('', pd.NA)
+    return df
+    
+
+
+def cleanData(filePath, userInput):
+    
+    #Convert CSV into a data frame
+    rawPath = "raw_data/" + filePath
+    df = pd.read_csv(rawPath, engine='pyarrow')
+    
+    #Case to create a new column for primary key
+    primaryKeyIndex = []
+    
+
+    #Remove column if its not used in database
+    for columnName, content in df.items():
+        if not(columnName in CSVColumns[userInput]):
+            df.drop([columnName], axis=1)
+    
+    
+    #Case to create a new column for primary key
+    if userInput == 2:
+        primaryKeyIndex.extend(range(0,len(df)))
+        df.insert(0,'status_id',primaryKeyIndex)
+
+    #Clean Data
+    match userInput:
+        case 0:
+            df = convertToNum('id', df)
+            #drop duplicate values
+            df = df.drop_duplicates(subset=['id'])
+
+            df = emptyToNaN('name', df)
+            df = convertToNum('dock_count', df)
+            df = convertToDate('installation_date', df, False)
+        case 1:
+            df = convertToNum('id', df)
+            df = df.drop_duplicates(subset=['id'])
+
+            df = convertToNum('lat', df)
+            df = convertToNum('long', df)
+            df = emptyToNaN('city', df)
+        case 2:
+            df = convertToNum('station_id', df)
+            df = df.drop_duplicates(subset=['station_id'])
+            df = convertToDate('time', df, True)
+            df = convertToNum('bikes_available', df)
+            df = convertToNum('docks_available', df)
+        case 3:
+            df = convertToNum('id', df)
+            df = df.drop_duplicates(subset=['id'])
+            
+            df = convertToNum('bike_id', df)
+            df = convertToNum('duration', df)
+            df = convertToNum('start_station_id', df)
+            df = convertToNum('end_station_id', df)
+            df = convertToDate('start_date', df, True)
+            df = convertToDate('end_date', df, True) 
+            
+            
+            #filter to only bike_id's in User.csv
+            dfUser = pd.read_csv('clean_data/User.csv', engine='pyarrow')
+            df = df[df['bike_id'].isin(dfUser['bike_id'])]
+
+            #filter to only station_id's in station.csv
+            dfStation = pd.read_csv('clean_data/station.csv', engine='pyarrow')
+            df = df[df['start_station_id'].isin(dfStation['id'])]
+            df = df[df['end_station_id'].isin(dfStation['id'])]
+        case 4:
+            df = convertToNum('bike_id', df)
+            df = df.drop_duplicates(subset=['bike_id'])
+            df = emptyToNaN('user_type', df)
+            df = emptyToNaN('member_birth_year', df)
+            df = emptyToNaN('member_gender', df)
+        case 5:
+            df = convertToDate('date', df, False)
+            df = df.drop_duplicates(subset=['date'])
+
+            df = convertToNum('max_temperature_f', df)
+            df = convertToNum('min_temperature_f', df)
+            df = convertToNum('max_visibility_miles', df)
+            df = emptyToNaN('events', df)
+
+
+    #reorder table
+    df = df.loc[:, CSVColumns[userInput]]
+
+    #connect weather to status
+    if userInput == 2:
+        df2 = df['time'].copy()
+        df2 = df2.to_frame(name='date_with_time')
+
+        df3 = df2['date_with_time'].copy()
+        df3 = df3.to_frame(name='date_without_time')
+        df3 = convertToDate('date_without_time', df3, False)
+        
+        df4 = pd.DataFrame()
+        df4['date_with_time'] = df2['date_with_time'].copy()
+        df4['date_without_time'] = df3['date_without_time'].copy()
+        df4.to_csv('clean_data/connect_Weather_Status.csv', index=False)
+        
+
+
+
+    #rewrite old csv with new one
+    if (userInput == 1):
+        filePath = 'station_location.csv'
+
+
+
+    cleanPath = 'clean_data/' + filePath
+    df.to_csv(cleanPath, index=False)
+
+
+
+
+#Have User choose which CSV to clean for loading
+print("Which CSV do you wish to clean\nstation (1)\nstation_location (2)\nstatus (3)")
+print("trip (4)\nuser (5)\nweather(6)\nall (7)")
+
+userInput = int(input(">> "))
+match userInput:
+    case 1:
+        filePath = 'station.csv'
+    case 2:
+        filePath = 'station.csv'
+    case 3:
+        filePath = 'status.csv'
+    case 4:
+        filePath = 'trip.csv'
+    case 5:
+        filePath = 'User.csv'
+    case 6:
+        filePath = 'weather.csv'
+    case 7:
+        filePath = 'not needed'
+    case _:
+        print("Incorrect input")
+
+if userInput > 0 and userInput < 7:
+    cleanData(filePath, userInput - 1)
+elif userInput == 7:
+    cleanData('station.csv', 0)
+    cleanData('station.csv', 1)
+    cleanData('status.csv', 2)
+    cleanData('User.csv', 4)
+    cleanData('trip.csv', 3)
+    cleanData('weather.csv', 5)
